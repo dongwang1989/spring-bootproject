8b9a0f2de799f574c0d1488b02d363b2cc79e8f7
wangdong <1819591996@qq.com>
2018-11-21 13:53:17 +0800

    更改1

--- src/main/java/cn/zzdz/error/EnumError.java
+++ src/main/java/cn/zzdz/error/EnumError.java
@@ -15,38 +15,24 @@ import java.util.Locale;
 
 public class EnumError extends RuntimeException {
 
-    @Autowired
-    private I18nController i18nController;
-
-    @Autowired
-    private MyLocaleResolver mylocale;
+//    @Autowired
+//    private I18nController i18nController;
+//
+//    @Autowired
+//    private MyLocaleResolver mylocale;
     private static final long serialVersionUID = 1L;
     private  String values;
 
-    public EnumError(String bb,String aa) {
-        MyLocaleResolver ft =new MyLocaleResolver();
-        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
-        Locale locale = ft.resolveLocale(request);
-        values = MessageSourceHolder.getMessageSource().getMessage(bb, null, locale);
-        values= MessageFormat.format(values,aa);
-        System.out.println(values);
-    }
+
     public EnumError(INotifyMessage notifyMessage,final String ... param) {
         MyLocaleResolver ft =new MyLocaleResolver();
         HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
         Locale locale = ft.resolveLocale(request);
         String mg=notifyMessage.getName();
         values = MessageSourceHolder.getMessageSource().getMessage(mg, null, locale);
-        values= MessageFormat.format(values,param[0]);
+        values= MessageFormat.format(values,param);
         System.out.println(values);
     }
-//    public EnumError(final INotifyMessage message, final String... params) {
-//        MyLocaleResolver ft =new MyLocaleResolver();
-//        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
-//        ft.resolveLocale(request);
-//        //String msg=i18nController.getMge(httpServletRequest,message.getName(),params[0]);
-//        values = MessageSourceHolder.getMessageSource().getMessage(message.getName(), params, null);
-//    }
     @Override
     public String getMessage() {
         final ResultDto resultdto = new ResultDto();
