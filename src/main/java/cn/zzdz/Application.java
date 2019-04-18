bd194b991167dffe9b016f21f4c3d599dc7a87a4
wangdong <1819591996@qq.com>
2019-04-18 16:06:02 +0800

    123

--- src/main/java/cn/zzdz/Application.java
+++ src/main/java/cn/zzdz/Application.java
@@ -1,5 +1,6 @@
 package cn.zzdz;
 
+import com.github.tobato.fastdfs.FdfsClientConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -8,6 +9,9 @@ import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableMBeanExport;
+import org.springframework.context.annotation.Import;
+import org.springframework.jmx.support.RegistrationPolicy;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 
 import cn.zzdz.config.WebMvcConfiguration;
@@ -15,6 +19,8 @@ import cn.zzdz.config.WebMvcConfiguration;
 @SpringBootApplication//(exclude = {DataSourceAutoConfiguration.class})
 @EnableCaching
 @MapperScan(value = "cn.zzdz.mapper")
+@Import(FdfsClientConfig.class)
+@EnableMBeanExport(registration = RegistrationPolicy.IGNORE_EXISTING)
 public class Application extends WebMvcConfiguration {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- src/main/java/cn/zzdz/usercontroller/Tet.java
+++ src/main/java/cn/zzdz/usercontroller/Tet.java
@@ -1,30 +1,40 @@
 package cn.zzdz.usercontroller;
 
 import cn.zzdz.dto.ResultDto;
+import cn.zzdz.fastfd.FastDFSClient;
 import cn.zzdz.interfaces.service.IUserService;
+import com.github.tobato.fastdfs.domain.MataData;
+import com.github.tobato.fastdfs.domain.StorePath;
+import com.github.tobato.fastdfs.service.FastFileStorageClient;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
-@Service
+//@Service
 @RestController
 @Api("用户相关操作")
 @RequestMapping("/tet")
 public class Tet {
+    @Autowired
+    private FastFileStorageClient fastFileStorageClient;
     @Autowired
     private IUserService userService;
     @ApiOperation(value = "登陆",httpMethod = "get",notes = "根据传过来的用户名和密码查询")
@@ -44,9 +54,32 @@ public class Tet {
         }
         return userService.login(username, pwd, cook);
     }
+
+
+    @ApiOperation(value = "文件上传",httpMethod = "post")
+    @PostMapping(value="/uppload",headers = "content-type=multipart/form-data")
+    public StorePath test(@RequestParam MultipartFile file) throws IOException {
+
+
+        // 设置文件信息
+        Set<MataData> mataData = new HashSet<>();
+        mataData.add(new MataData("author", "zonghui"));
+        mataData.add(new MataData("description", "xxx文件，嘿嘿嘿"));
+
+        // 上传   （文件上传可不填文件信息，填入null即可）
+        StorePath storePath = fastFileStorageClient.uploadFile(file.getInputStream(), file.getSize(), FilenameUtils.getExtension(file.getOriginalFilename()), mataData);
+    String url=FastDFSClient.getResAccessUrl( storePath.getFullPath());
+    System.out.println("address dizhi:"+url);
+        return storePath;
+//        String fileUrl = this.getClass().getResource("/test.jpg").getPath();
+//        File file = new File(fileUrl);
+//        String str = FastDFSClient.uploadFile(file);
+
+    }
+
     @RequestMapping("/c")
-    public void gec(String id){
-        System.out.println(id);
+    public void gec(){
+       boolean a= FastDFSClient.downloadFile2("group1/M00/00/00/rBBkLFy4GLOAKGzJAAMtk3_3ZEM552.jpg","/Users/wangdong/Downloads/a.jpg");
     }
     @RequestMapping("/cc")
     @CacheEvict(value="producta",key="#id")// 清空accountCache 缓存
